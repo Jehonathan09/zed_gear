22ba11204b8dde74226f98cefe242c2e8b399920
Jehonathan09 <112160041+Jehonathan09@users.noreply.github.com>
2024-06-01 01:32:02 +0200

    "Updated CustomButton component to accept props and added type definitions; refactored Hero component to pass props to CustomButton"

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,29 @@
 "use client"
-
 import Image from "next/image";
+import { CustomButtonProps } from "@/types";
 
-const CustomButton = () => {
+
+//Define the CustomButton compoment
+//This component renders a custom button with a title and a optional click handler 
+const CustomButton = ({title, containerStyles, 
+    handleClick}: CustomButtonProps) => {
+   // Render the button element with the given props
+  // The button is disabled by default and has a type of "button"
+  // The className prop is set to "custom-btn" and the containerStyles prop
   return (
     <button
     disabled={false}
     type={"button"}
-    className={'custom-btn'}
-    onClick={() => {}}
+    className={'custom-btn ${containerStyles}'}
+    onClick={handleClick}
     >
+        {/* Render the title prop inside a span element with a flex-1 class */}
         <span className={'flex-1'}>
-            title
+            {title}
         </span>
 
     </button>
   )
 }
-
+// Export the CustomButton component
 export default CustomButton
